4894b0a9885c904e28651d5b56ab4b808d29fb4f
john <johnpwoodiv@gmail.com>
2017-09-06 14:24:01 -0500

    added chat functionality

--- js/messageService.js
+++ js/messageService.js
@@ -1,11 +1,15 @@
 angular.module('chatroom').service('messageService', function($http){
+  var self = this;
+  var url = 'https://practiceapi.devmountain.com/api/chats';
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
 
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'https://practiceapi.devmountain.com/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
 
-
+  this.getMessages = function(){
+    return $http.get(url)
+  }
 
 
   //On the line below create the postMessage method. This method will add data to the backend server.
@@ -13,7 +17,9 @@ angular.module('chatroom').service('messageService', function($http){
   //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
   //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
 
-
+  this.postMessage = function(m){
+    return $http.post(url, {message: m})
+  }
 
 
 });
